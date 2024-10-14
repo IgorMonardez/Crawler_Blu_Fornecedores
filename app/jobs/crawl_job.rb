988845fe1a13d305f76677710907aa6871a89070
IMonardez <igormonardez@id.uff.br>
2024-10-14 12:12:22 -0300

    Criando o escopo do serviço de crawler

---
+++ app/jobs/crawl_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class CrawlJob < ApplicationJob
+
+  def perform(url)
+
+  end
+end
---
+++ app/services/web_crawler.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class WebCrawler
+
+  def initialize(url)
+    @url = url
+  end
+
+  def extract_data
+
+  end
+end
