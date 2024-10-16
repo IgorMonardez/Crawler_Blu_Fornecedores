e5cab6a0834002b5633ef620eb5201f985431f33
IMonardez <igormonardez@id.uff.br>
2024-10-16 12:09:22 -0300

    Filtragem dos fornecedores

--- app/controllers/fornecedores_controller.rb
+++ app/controllers/fornecedores_controller.rb
@@ -1,6 +1,26 @@
 class FornecedoresController < ApplicationController
+
+
+
   def index
-    @fornecedor = Fornecedor.all
-    render json: @fornecedor
+    @fornecedores = Fornecedor.all
+
+    @fornecedores = filtra_fornecedores(@fornecedores, filtro_params)
+    render json: @fornecedores
+  end
+
+  private
+
+  def filtra_fornecedores(fornecedores, filtro_params)
+    fornecedores = fornecedores.where("nome LIKE ?", "%#{params[:nome]}%") if params[:nome].present?
+    fornecedores = fornecedores.where(regiao_id: params[:regiao_id]) if params[:regiao_id].present?
+    fornecedores = fornecedores.where(segmento_id: params[:segmento_id]) if params[:segmentos_id].present?
+
+
+    fornecedores
+  end
+
+  def filtro_params
+    params.permit(:nome, :regiao_id, :segmento_id)
   end
 end
